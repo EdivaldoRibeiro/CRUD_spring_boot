d74f5d50423b687bfdc708b30db6bfb7c9217e05
Edivaldo Ribeiro <java.betel@uol.com.br>
2018-12-09 20:01:53 -0200

    Modelo de CRUD usando spring boot

---
+++ src/main/java/br/com/edivaldo/teste5/Boot.java
@@ -0,0 +1,14 @@
+package br.com.edivaldo.teste5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot
+{
+
+   public static void main(String[] args)
+   {
+      SpringApplication.run(Boot.class, args);
+   }
+}
---
+++ src/main/java/br/com/edivaldo/teste5/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package br.com.edivaldo.teste5.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.com.edivaldo.teste5.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+}
